6a8d61cc08b26b0111a0a7712cd8fc17cc27ab7b
Mastermind <148354429+GibsonHolben@users.noreply.github.com>
2024-03-18 14:42:02 -0400

    Improved test case

--- src/Main.java
+++ src/Main.java
@@ -2,40 +2,64 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static void main(String[] args){
+    static boolean keepGoing = true;
+    static Encryptor Encryptor;
+    static void changeSeed()
+    {
         Scanner sc = new Scanner(System.in);
-        String enced = "";
+        System.out.println("Please enter a encryption/decryption key (Remember the numbers)");
+        String Seed = sc.nextLine();
+        if(Seed.isEmpty() || Seed.equals(" ")){Seed = "0";}
+        Encryptor = new Encryptor(Integer.parseInt(Seed));
+    }
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        String encrypted = "";
 
         System.out.println("Please enter a encryption/decryption key (Remember the numbers)");
 
         String Seed = sc.nextLine();
         if(Seed.isEmpty() || Seed.equals(" ")){Seed = "0";}
-        Encryptor Encryptor = new Encryptor(Integer.parseInt(Seed));
+        Encryptor = new Encryptor(Integer.parseInt(Seed));
+        while(keepGoing) {
 
-        System.out.println("encrypt or decrypt? (enc/dec)");
-        Encryptor.setSpaceChar("_");
-        Encryptor.setLowerChar("*");
-        sc = new Scanner(System.in);
-        switch (sc.nextLine())
-        {
-            case "enc":
-                System.out.println("Enter a string to encrypt");
-                String toEnc = sc.nextLine();
-                enced = Encryptor.encrypt(toEnc);
-                System.out.println(enced);
-                break;
-            case "dec":
-                System.out.println("Enter a string to decrypt");
-                String deced = Encryptor.decrypt(sc.nextLine());
-                System.out.println(deced);
-                break;
-            case "dump":
-                for(int i : Encryptor.letterNumb)
-                {
-                    System.out.println(i + "," + Encryptor.Letters.get(i-1));
-                }
 
+            System.out.println("what would you like to do? (enc/dec/help/exit)");
+            Encryptor.setSpaceChar("_");
+            Encryptor.setLowerChar("*");
+            sc = new Scanner(System.in);
+            switch (sc.nextLine().toLowerCase())
+            {
+                case "enc":
+                    System.out.println("Enter a string to encrypt");
+                    String toEnc = sc.nextLine();
+                    encrypted = Encryptor.encrypt(toEnc);
+                    System.out.println(encrypted);
+                    break;
+                case "dec":
+                    System.out.println("Enter a string to decrypt");
+                    String decrypted = Encryptor.decrypt(sc.nextLine());
+                    System.out.println(decrypted);
+                    break;
+                case "dump":
+                    for(int i : Encryptor.letterNumb)
+                    {
+                        System.out.println(i + "," + Encryptor.Letters.get(i-1));
+                    }
+                    break;
+                case "exit":
+                    keepGoing = false;
+                    break;
+                case "seed":
+                    changeSeed();
+                    break;
+                default:
+                    System.out.println("'" + sc.nextLine() + "' is not recognized as a command");
+
+            }
         }
+
 
     }
 }
